4ea803396198e8e8a27361e1853e788349826459
Gopistol <104254012+Gopistol@users.noreply.github.com>
2024-04-13 17:29:17 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 852 ms, Memory: 71044 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -18,39 +18,25 @@ for _ in range(M):
     graph[u].append(v)
     graph[v].append(u)
 
-for key in graph:
-    graph[key].sort()
 
-
-def dfs(start, graph):
-    visited = []
+def dfs(start, graph, visited):
     to_visit = deque([start])
 
     while to_visit:
         node = to_visit.pop()
-
-        if node not in visited:
-            visited.append(node)
+        if not visited[node]:
+            visited[node] = True
             if node not in graph:
                 return
             to_visit.extend(graph[node])
-
-    return visited
 
 
 v_visited = [False for _ in range(N + 1)]
 count = 0
 
 for i in range(1, N + 1):
-
     if not v_visited[i]:
-        vertexes = dfs(i, graph)
-        if not vertexes:
-            count += 1
-            continue
-        for v in vertexes:
-            v_visited[v] = True
-
+        dfs(i, graph, v_visited)
         count += 1
 
 print(count)
