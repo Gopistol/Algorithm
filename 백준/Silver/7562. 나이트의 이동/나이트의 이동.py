651bd681c8524fd9de98512076436082844b4034
Gopistol <104254012+Gopistol@users.noreply.github.com>
2024-04-18 19:36:59 +0900

    [Silver I] Title: 나이트의 이동, Time: 2648 ms, Memory: 34068 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -3,52 +3,40 @@ from collections import deque
 
 input = sys.stdin.readline
 
-# 방향 8개
 dx = [-2, -1, 1, 2, -2, -1, 1, 2]
 dy = [1, 2, 2, 1, -1, -2, -2, -1]
 
 T = int(input())
 
-
-# board의 각 칸마다 나이트가 몇 번을 이동해야 올 수 있는지
-# 최소 count를 저장
-
-
-def bfs(start, end, board):
-    queue = deque([start])
-    e_i, e_j = end
-
+def bfs(start, end, l):
     if start == end:
         return 0
+
+    board = [[-1 for _ in range(l)] for _ in range(l)]
+
+    queue = deque([start])
+    board[start[0]][start[1]] = 0
 
     while queue:
         x, y = queue.popleft()
 
         for i in range(8):
-            n_x = x + dx[i]
-            n_y = y + dy[i]
+            n_x, n_y = x + dx[i], y + dy[i]
 
-            if n_x < 0 or n_x >= len(board) or n_y < 0 or n_y >= len(board):
-                continue
-
-            if board[n_x][n_y] > 0:
-                continue
-
-            if n_x == e_i and n_y == e_j:
+            if 0 <= n_x < l and 0 <= n_y < l and board[n_x][n_y] == -1:
                 board[n_x][n_y] = board[x][y] + 1
-                break
 
-            board[n_x][n_y] = board[x][y] + 1
-            queue.append((n_x, n_y))
+                if (n_x, n_y) == end:
+                    return board[n_x][n_y]
 
-    return board[e_i][e_j]
+                queue.append((n_x, n_y))
+
+    return board[end[0]][end[1]]
 
 
 for _ in range(T):
     l = int(input())
     start = list(map(int, input().split()))
     end = list(map(int, input().split()))
-    board = [[0 for _ in range(l)] for _ in range(l)]
 
-    print(bfs(start, end, board))
- 
+    print(bfs(start, end, l))
