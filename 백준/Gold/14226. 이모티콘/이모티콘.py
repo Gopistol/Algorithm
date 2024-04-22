432cef872ebc63ec6ed1770890418520d77cc89f
Gopistol <104254012+Gopistol@users.noreply.github.com>
2024-04-22 15:22:56 +0900

    [Gold IV] Title: 이모티콘, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.py
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -1,13 +1,10 @@
 import sys
-
 s = int(sys.stdin.readline())
 d = list(range(1002))
-
 for i in range(2, s + 1):
     j = 2
     while i * j < 1002:
         d[i * j] = min(d[i * j], d[i] + j)
         d[i * j - 1] = min(d[i * j - 1], d[i * j] + 1)
         j += 1
-
 print(d[s])
