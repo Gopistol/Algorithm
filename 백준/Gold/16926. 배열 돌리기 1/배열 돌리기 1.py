9989edc28f683ebaca3569db3873a6adbeeca69d
Gopistol <104254012+Gopistol@users.noreply.github.com>
2024-04-24 19:20:46 +0900

    [Gold V] Title: 배열 돌리기 1, Time: 76 ms, Memory: 36372 KB -BaekjoonHub

--- 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.py
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.py
@@ -3,15 +3,14 @@ import sys
 input = sys.stdin.readline
 
 n, m, r = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
+arr = [input().split() for _ in range(n)]
 
 
 def rotate_layer(layer, r):
     total_elements = 2 * (n - 2 * layer) + 2 * (m - 2 * layer - 2)
-    r = r % total_elements  # 실제로 회전해야 하는 횟수를 최소화
+    r = r % total_elements 
     elements = []
 
-    # 현재 층의 요소를 순서대로 추출
     for i in range(layer, m - layer):
         elements.append(arr[layer][i])
     for i in range(layer + 1, n - layer):
@@ -21,10 +20,8 @@ def rotate_layer(layer, r):
     for i in range(n - layer - 2, layer, -1):
         elements.append(arr[i][layer])
 
-    # 회전 처리
     rotated_elements = elements[r:] + elements[:r]
 
-    # 회전된 요소 배치
     idx = 0
     for i in range(layer, m - layer):
         arr[layer][i] = rotated_elements[idx]
@@ -43,5 +40,5 @@ def rotate_layer(layer, r):
 for layer in range(min(n, m) // 2):
     rotate_layer(layer, r)
 
-for row in arr:
-    print(*row)
+for i, row in enumerate(arr):
+    print(' '.join(row))
